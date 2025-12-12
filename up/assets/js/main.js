654886390ea6e3fe73bd32d46137160de9309b66
lapgia57-netizen <lapgia57@gmail.com>
2025-12-12 10:09:37 +0700

    Create main.js

---
+++ up/assets/js/main.js
@@ -0,0 +1,9 @@
+// Hiệu ứng mượt khi cuộn đến section
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
